0c95c4911089221645d72224a329c8759d6acbf7
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-07-13 19:00:45 +0000

    Deploy sdk.hardwario.com to github.com/hardwario/bcf-sdk.git:gh-pages

---
+++ group__bc__ds18b20.js
@@ -0,0 +1,19 @@
+var group__bc__ds18b20 =
+[
+    [ "bc_ds18b20_t", "group__bc__ds18b20.html#ga27869dad80eb488e6dedea43a9ff7bb3", null ],
+    [ "bc_ds18b20_event_t", "group__bc__ds18b20.html#ga96c2af9f518eb44f3027e18a4e90ffba", [
+      [ "BC_DS18B20_EVENT_ERROR", "group__bc__ds18b20.html#gga96c2af9f518eb44f3027e18a4e90ffbaaa0655a46cc7b700a83b55c733a7c085d", null ],
+      [ "BC_DS18B20_EVENT_UPDATE", "group__bc__ds18b20.html#gga96c2af9f518eb44f3027e18a4e90ffbaa62058d2052aefdcbad6a93244770ba5e", null ]
+    ] ],
+    [ "bc_ds18b20_get_index_by_device_address", "group__bc__ds18b20.html#ga438239b0eb96a5793f4dbdb5bd7dd072", null ],
+    [ "bc_ds18b20_get_sensor_found", "group__bc__ds18b20.html#ga286dd385e0cbd97de07a9040d51e87a0", null ],
+    [ "bc_ds18b20_get_temperature_celsius", "group__bc__ds18b20.html#gaa9f6f0de5860698b318e41ad5da9834c", null ],
+    [ "bc_ds18b20_get_temperature_raw", "group__bc__ds18b20.html#gaed755b4a626de84914d98faaf3a4a39d", null ],
+    [ "bc_ds18b20_init", "group__bc__ds18b20.html#ga30a4a322a424ed0607a456709c7a67ca", null ],
+    [ "bc_ds18b20_init_multiple", "group__bc__ds18b20.html#gad21710a9d11d05eefb66a20ea24a9b19", null ],
+    [ "bc_ds18b20_init_single", "group__bc__ds18b20.html#gab43e69047d9129429e9bd7237b0db171", null ],
+    [ "bc_ds18b20_measure", "group__bc__ds18b20.html#ga5bb61ed139ddd1e0206dcddb5dc72e9d", null ],
+    [ "bc_ds18b20_set_event_handler", "group__bc__ds18b20.html#ga742a2b8d5fae5686a8243bdc59682b7c", null ],
+    [ "bc_ds18b20_set_power_dynamic", "group__bc__ds18b20.html#ga96bc537356bec3fd560a5ec99b668877", null ],
+    [ "bc_ds18b20_set_update_interval", "group__bc__ds18b20.html#gaa351a0f2746b02bb3b6edeff8a98d80e", null ]
+];
---
+++ navtreeindex9.js
@@ -0,0 +1,24 @@
+var NAVTREEINDEX9 =
+{
+"structminmea__sentence__rmc.html#a5674b8d1ba4da61ffe2e9dad9b929330":[2,0,72,4],
+"structminmea__sentence__rmc.html#a72d9f4f5591e8203130a3c713d8b1ea5":[2,0,72,2],
+"structminmea__sentence__rmc.html#a9b2ea3636a3da468032836c98daf7e31":[2,0,72,6],
+"structminmea__sentence__rmc.html#ad665cdb51a3a7833b600e9c3d5bd4b66":[2,0,72,5],
+"structminmea__sentence__rmc.html#adf96bf0ec3aaf7f61ff971525b759e46":[2,0,72,0],
+"structminmea__sentence__vtg.html":[2,0,73],
+"structminmea__sentence__vtg.html#a159190c573514f03c7001ca3e6c36156":[2,0,73,0],
+"structminmea__sentence__vtg.html#a1a4769bde9b9cefd8d2611825534875b":[2,0,73,1],
+"structminmea__sentence__vtg.html#a20f2b5aaedf76de69e129bf333edc497":[2,0,73,3],
+"structminmea__sentence__vtg.html#a35a0c435c550a933dfba49a0cf97d1a3":[2,0,73,4],
+"structminmea__sentence__vtg.html#a685743058795f7eeb34d2de5e7c2d643":[2,0,73,2],
+"structminmea__sentence__zda.html":[2,0,74],
+"structminmea__sentence__zda.html#a6026358d3e6a27ab95c1fd09819f7f6a":[2,0,74,0],
+"structminmea__sentence__zda.html#abf45d8f857c483783330879df6d2d12e":[2,0,74,2],
+"structminmea__sentence__zda.html#ad47bdc73c167b9cc9ffde465c8741095":[2,0,74,3],
+"structminmea__sentence__zda.html#af76b296fb15e38c23ed1aef15f80f96a":[2,0,74,1],
+"structminmea__time.html":[2,0,75],
+"structminmea__time.html#a4923c7bf39816194e5de3a4eedddf480":[2,0,75,2],
+"structminmea__time.html#a58234021423887e5c628fb691decd789":[2,0,75,1],
+"structminmea__time.html#a826ce4cae9eff6c6c595d9b3b48a488a":[2,0,75,3],
+"structminmea__time.html#aca5af0070d82947d29a535e6c0e6411a":[2,0,75,0]
+};
